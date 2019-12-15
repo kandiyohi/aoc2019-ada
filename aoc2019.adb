b6cf2599fe09f9d03783d59c1e995eb2816343cf
Donny Johnson <kandiyohi.snow@gmail.com>
2019-12-15 10:43:55 -0600

    Clean up main function and add comments.

--- aoc2019.adb
+++ aoc2019.adb
@@ -1,15 +1,8 @@
 with Ada.Text_IO;
-
-with Ada.Text_IO.Unbounded_IO;
+use Ada.Text_IO;
 
 with Ada.Strings.Unbounded;
 use Ada.Strings.Unbounded;
-
-with Ada.Directories;
-
-with Day_1;
-
-with Ada.Containers.Doubly_Linked_Lists;
 
 with Get_Integer_Input;
 use Get_Integer_Input;
@@ -17,27 +10,20 @@ use Get_Integer_Input;
 with Get_Input_File;
 use Get_Input_File;
 
+with Day_1;
+
 function aoc2019 return Integer is
-	-- It's important to distinguish between Text_IO and Unbounded_IO.  For
-	-- printing, Text_IO is easier to user, but for input, Unbounded_IO is far
-	-- easier to use.
 	package Text_IO renames Ada.Text_IO;
-	package Unbounded_IO renames Ada.Text_IO.Unbounded_IO;
-	package Directories renames Ada.Directories;
-	package Unbounded_String_List is new Ada.Containers.Doubly_Linked_Lists (Unbounded_String);
-	package Integer_IO is new Text_IO.Integer_IO (Integer);
 
 	type Procedure_To_Run_Type is access procedure(Input_File_Path: String);
 
 	Procedure_To_Run: Procedure_To_Run_Type;
-	Day_To_Run: Unbounded_String;
 	Input_File_Name: Unbounded_String;
-	File_Name: Unbounded_String;
-	Response: Unbounded_String;
-	Input_Directory: Unbounded_String := To_Unbounded_String("inputs/");
 	Day: Integer;
 	Input_File_Pattern: Unbounded_String;
 begin
+	-- Get which day we should run.  We should eventually give a list of all
+	-- days we can support.
 	Day := Get_Integer("Specify which day to solve: ");
 	case Day is
 		when 1 =>
@@ -48,13 +34,16 @@ begin
 			return 1;
 	end case;
 
+	-- Get which input file to use based on the day from inputs/.
 	Input_File_Name := Get_Input_File.Get_Input_File("Input files: ", "Select input files: ", "inputs/", "day_1*.txt");
-
 	if Input_File_Name = To_Unbounded_String("") then
 		Text_IO.Put_Line("ERROR: No file name selected.");
 		Text_IO.Put_Line("Please retry program and select a valid option.");
 		return 1;
 	end if;
+
+	-- Run the selected day procedure with the input file.
 	Procedure_To_Run.all(To_String(Input_File_Name));
+
 	return 0;
 end;
