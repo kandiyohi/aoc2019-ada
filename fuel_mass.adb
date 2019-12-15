1b5cc6aa481472ba55debe232f55eed93280b2e2
Donny Johnson <kandiyohi.snow@gmail.com>
2019-12-15 14:10:47 -0600

    Reusing fuel mass calculation in recursive calculation.

--- fuel_mass.adb
+++ fuel_mass.adb
@@ -1,9 +1,9 @@
 package body Fuel_Mass is
 	function Calculate_Fuel_Mass_Recursive(Mass: Integer) return Integer is
-		Calculated_Mass: Integer := Mass / 3 - 2;
+		Calculated_Mass: Integer := Calculate_Fuel_Mass(Mass);
 	begin
-		if Calculated_Mass <= 0 then
-			return 0;
+		if Calculated_Mass = 0 then
+			return Calculated_Mass;
 		end if;
 		return Calculated_Mass + Calculate_Fuel_Mass_Recursive(Calculated_Mass);
 	end Calculate_Fuel_Mass_Recursive;
